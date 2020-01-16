28f51a84bf2fa3bcecb0bd6a2a28eb2081fd0041
john-ngarambe <51552292+john-ngarambe@users.noreply.github.com>
2020-01-16 13:35:17 -0500

    string challeanges

---
+++ src/exercise/collections/java/Main.java
@@ -0,0 +1,15 @@
+package exercise.collections.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+    String s1 ="ABCD";
+    String s2 ="Ac";
+    if(s2.contains("AB")){
+        System.out.println("S2 is sub string of s1");
+    }else{
+        System.out.println("It does't");
+    }
+
+}
+}
